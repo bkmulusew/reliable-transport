9df7d79e5a636165cc72eefef5af234708840d7e
bkmulusew <beakalmulusew@gmail.com>
2026-01-05 19:00:52 -0500

    implement UDP packet chunking and in-order reassembly

--- streamer.py
+++ streamer.py
@@ -2,7 +2,12 @@
 from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
+import struct
 
+MAX_UDP_PAYLOAD = 1472
+HEADER_FMT = "!HHH"          # msg_id, seq, total
+HEADER_SIZE = struct.calcsize(HEADER_FMT)
+MAX_DATA_SIZE = MAX_UDP_PAYLOAD - HEADER_SIZE
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -14,21 +19,71 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
+        self._next_msg_id = 0  # increments per send()
+
+        # Receiver state
+        self._expected_msg_id = 0
+        self._inflight = {}   # msg_id -> {"total": int, "chunks": {seq: bytes}}
+        self._complete = {}   # msg_id -> bytes
+
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        # Your code goes here!  The code below should be changed!
+        msg_id = self._next_msg_id & 0xFFFF
+        self._next_msg_id = (self._next_msg_id + 1) & 0xFFFF
 
-        # for now I'm just sending the raw application-level data in one UDP payload
-        self.socket.sendto(data_bytes, (self.dst_ip, self.dst_port))
+        total = (len(data_bytes) + MAX_DATA_SIZE - 1) // MAX_DATA_SIZE
+
+        for seq in range(total):
+            start = seq * MAX_DATA_SIZE
+            chunk = data_bytes[start:start + MAX_DATA_SIZE]
+            header = struct.pack(HEADER_FMT, msg_id, seq, total)
+            self.socket.sendto(header + chunk, (self.dst_ip, self.dst_port))
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        """Blocks until the next in-order message (by send-call) is ready."""
+        # If we already have the next message fully assembled, return it immediately
+        if self._expected_msg_id in self._complete:
+            data = self._complete.pop(self._expected_msg_id)
+            self._expected_msg_id = (self._expected_msg_id + 1) & 0xFFFF
+            return data
+
+        while True:
+            packet, addr = self.socket.recvfrom()
+
+            # Basic sanity: must at least contain header
+            if len(packet) < HEADER_SIZE:
+                continue
+
+            msg_id, seq, total = struct.unpack(HEADER_FMT, packet[:HEADER_SIZE])
+            payload = packet[HEADER_SIZE:]
+
+            # Create inflight entry if first time seeing this msg_id
+            st = self._inflight.get(msg_id)
+            if st is None:
+                st = {"total": total, "chunks": {}}
+                self._inflight[msg_id] = st
+            else:
+                # Sanity: ignore inconsistent 'total' for same msg_id
+                if st["total"] != total:
+                    continue
+
+            if seq >= total:
+                continue
+
+            # Store chunk if new (ignore duplicates)
+            st["chunks"].setdefault(seq, payload)
+
+            # If this message is now complete, assemble and stash it
+            if len(st["chunks"]) == st["total"]:
+                assembled = b"".join(st["chunks"][i] for i in range(st["total"]))
+                self._complete[msg_id] = assembled
+                del self._inflight[msg_id]
+
+                # If it’s the next expected message, return it now
+                if msg_id == self._expected_msg_id:
+                    data = self._complete.pop(self._expected_msg_id)
+                    self._expected_msg_id = (self._expected_msg_id + 1) & 0xFFFF
+                    return data
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
